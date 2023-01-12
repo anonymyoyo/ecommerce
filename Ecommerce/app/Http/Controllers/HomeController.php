cc3848748759f5ada9e928d259700ef8fc7e0a69
anonymyoyo <paulemmanuelbohole5@gmail.com>
2023-01-12 13:46:23 +0100

    multi authentication

---
+++ Ecommerce/app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    //
+    public function redirect()
+    {
+
+        $usertype = Auth::User()->usertype;
+
+        if($usertype=='1')
+        {
+
+            return view('admin.home');
+        }
+
+        else
+        {
+            return view('dashboard');
+        }
+    }
+}
